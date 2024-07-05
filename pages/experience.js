b4cfcddb5ccc0f233187675dbe6bfe794abd4325
oliviawilcox1 <oliviawilcox07@gmail.com>
2024-07-05 14:27:27 -0400

    Fix title name

--- pages/experience.js
+++ pages/experience.js
@@ -50,7 +50,7 @@ const Experience = () => {
   }
   return (
     <>
-      <PageSEO title={`Contact - ${siteMetadata.author}`} description="My Experience" />
+      <PageSEO title={`Experience - ${siteMetadata.author}`} description="My Experience" />
       <div className="space-y-2 pt-6 pb-8 md:space-y-5">
         <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
           Experience
